36ba6c5d52c8f99d8e5a7f2967d21c1e4ea28bba
Phaesphoros <Phaesphoros@lavabit.com>
2013-07-26 17:33:05 +0200

    A first test of the data structure / format and sparsely populated arrays.

---
+++ mech-variants.js
@@ -0,0 +1,29 @@
+var mechs =
+{
+	"JR7":
+	{
+		maxTonnage:35,
+		variants:
+		{
+			  "D":{minEngine:100, maxEngine:300}
+			, "F":{minEngine:100, maxEngine:300}
+			, "K":{minEngine:100, maxEngine:300}
+			, "F(C)":{minEngine:100, maxEngine:300}
+			, "D(S)":{minEngine:100, maxEngine:300}
+		}
+	}
+
+	, "AS7":
+	{
+		maxTonnage:100,
+		variants:
+		{
+			  "D":{minEngine:200, maxEngine:360}
+			, "K":{minEngine:200, maxEngine:360}
+			, "RS":{minEngine:200, maxEngine:360}
+			, "D-DC":{minEngine:200, maxEngine:360}
+			, "RS(C)":{minEngine:200, maxEngine:360}
+		}
+	}
+};
+
